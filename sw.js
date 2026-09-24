7e7b6392d2e32690ac3c629a099550b86c7678ea
Alan GB <13211405229@uptex.edu.mx>
2026-09-24 13:45:40 -0600

    Archivos iniciales
    Creación del repositorio con archivos existentes

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'simulador-bancario-v3';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './Simulador-icon.png'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
+});
